23d77666cd5d424a7085973ca148d3f2588cf934
mallikarjun1592 <mallikarjun1592@gmail.com>
2024-09-18 00:54:09 +0530

    login component with regexp

--- SimpleAngular/src/app/app.component.ts
+++ SimpleAngular/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
+import { LoginComponent } from './login/login.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,EmployeeComponent],
+  imports: [CommonModule, RouterOutlet,EmployeeComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 
--- SimpleAngular/src/app/login/login.component.ts
+++ SimpleAngular/src/app/login/login.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  
+  name=new FormGroup(
+   { firstname: new FormControl('',Validators.required),
+      password: new FormControl('',[Validators.required,  Validators.pattern(/^(?=\D*\d)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z]).{8,30}$/)])
+   },
+   
+  ) 
+  onSubmit(){
+   // console.log(this.name.value.firstname) // get firstname from the form data;
+   localStorage.setItem('name', 'Mallikarjun'); // set locallstorage after login
+   
+   console.log(this.name.value)
+  }
 
 }
